cdb7599601ea77fa531ca62738e5418cc4d8c0fe
EPham42747 <ethancpham@gmail.com>
2025-07-07 15:49:21 -0400

    Attempted missing CSS fix

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: "/portfolio"
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio/",
 };
 
 export default nextConfig;
